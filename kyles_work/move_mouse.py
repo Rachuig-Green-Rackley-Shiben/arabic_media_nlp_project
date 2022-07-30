5f30ae49ff340c31a32c0634bbd1a30834432321
KyleTaylorGreen <kyle.taylor.green@gmail.com>
2022-07-29 21:25:04 -0500

    added move mouse script to keep computer active

---
+++ kyles_work/move_mouse.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+
+def main():
+    pyautogui.PAUSE = 15
+    pyautogui.FAILSAFE = True
+    while True:
+        pyautogui.moveTo(100, 200, 2)
+        pyautogui.moveTo(200,100, 2)
+
+main()
